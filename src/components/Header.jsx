9f82b5090e287a5e5c9f318a54f39efe873a2bd3
GigaBerdzenishvili <87497933+GigaBerdzenishvili@users.noreply.github.com>
2023-01-31 18:14:48 +0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header({ children }) {
     <div className="bg-indigo-400 flex items-center py-10">
       <div className="max-w-md mx-auto w-full">
         <h1 className="text-white font-mono text-center text-xl font-bold mb-5">
-          Search images from unspash
+          Search images
         </h1>
         {children}
       </div>
